b10d91728f829e45a72eecfc55338bcb2154d20c
Kevin <kevinalvaro.cunyasquispe@gmail.com>
2025-02-07 20:26:57 +0100

    Implement security configuration and update client controller with authorization BASIC - NO DB

---
+++ Infrastructure/src/main/java/com/school/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,94 @@
+package com.school.infrastructure.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  /*  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+    return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .httpBasic(Customizer.withDefaults())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            http -> {
+              // Configurar los endpoints publicos
+              http.requestMatchers(HttpMethod.GET, "clients/secureclient").permitAll();
+
+              // Configurar los endpoints privados
+              http.requestMatchers(HttpMethod.GET, "clients/list").hasAnyAuthority("create");
+
+              // Configurar el resto de endpoints - NO SE DEBE PERMITIR NINGUNA PETICION
+              http.anyRequest().denyAll();
+            })
+        .build();
+  }*/
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+    return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .httpBasic(Customizer.withDefaults())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(passwordEncoder());
+    provider.setUserDetailsService(userDetailsService());
+
+    return provider;
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+
+    List<UserDetails> userDetailsList = new ArrayList<>();
+
+    userDetailsList.add(User.withUsername("kevin").password("kevin").authorities("read").build());
+    userDetailsList.add(
+        User.withUsername("admin").password("admin").authorities("read", "write").build());
+
+    return new InMemoryUserDetailsManager(userDetailsList);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+}
