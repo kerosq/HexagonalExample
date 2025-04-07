a63b0841c100631d89f39dfdeeee8257293806d5
Kevin <kevinalvaro.cunyasquispe@gmail.com>
2025-04-07 23:17:31 +0200

    Implement GetUsersDelegate and add OpenAPI generator plugin in pom.xml

--- Infrastructure/rest/src/main/java/com/school/infrastructure/rest/controller/GetUsersDelegate.java
+++ Infrastructure/rest/src/main/java/com/school/infrastructure/rest/controller/GetUsersDelegate.java
@@ -1,13 +1,27 @@
 package com.school.infrastructure.rest.controller;
 
 import com.school.domain.usecase.GetUsersUseCase;
+import com.school.infrastructure.rest.api.UserApi;
 import com.school.infrastructure.rest.mapper.UserRestMapper;
+import com.school.infrastructure.rest.model.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class GetUsersDelegate {
+public class GetUsersDelegate implements UserApi {
   private final UserRestMapper userRestMapper;
   private final GetUsersUseCase getUsersUseCase;
+
+  @Override
+  public ResponseEntity<User> _getUserById(Long userId) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<List<User>> _getUsers() {
+    return null;
+  }
 }
